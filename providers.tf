d582588cfbd06e747499337bebd42880b44636af
Alex Salome <asalome01@gmail.com>
2022-05-18 14:22:20 -0500

    terraform vpc

--- providers.tf
+++ providers.tf
@@ -2,4 +2,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
-
+terraform {
+  backend "s3" {
+    bucket         = "javahome-jenkins01-asp01"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    #dynamodb_table = "javahome-tf"
+  }
+}
