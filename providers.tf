b8254ed42bf73efe51fd404ee52bf49ab68230bb
Alex Salome <asalome01@gmail.com>
2022-05-17 22:55:32 -0500

    pipeline

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "javahome-tf-12345678"
+    bucket         = "javahome-tf-12345678-asp01"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     #dynamodb_table = "javahome-tf"
